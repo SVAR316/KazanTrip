ea14733b1799dce6bcefb127c0d790044f60001b
Artem <dt2csgo@mail.ru>
2024-02-05 22:14:04 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,13 @@ import restaurant from './images/restaurant.png'
 import kazan from './images/kazan.png'
 import arrowRightLong from './images/arrowrightLong.svg'
 import location from './images/location.svg'
+import cartLounge from './images/cartLounge.png'
+import meraKitchen from  './images/meraKitchen.png'
+import azu from  './images/azu.png'
+import artel from './images/artel.png'
+import vanGofh from './images/vanGofh.png'
+import cheeseria from './images/cheeseria.png'
+import formCloud from './images/cloudForm.png'
 
 function App() {
 
@@ -28,7 +35,7 @@ function App() {
                             <a href="">Популярное</a>
                         </div>
                         <a className={"header-content__right"}>
-                            <img src={logo}/>
+                            <img src={logo} alt={'logo'}/>
                             KazanTrip
                         </a>
                     </div>
@@ -135,28 +142,82 @@ function App() {
                             <p className={'places-upper__text-upper'}>Интересные кафешки для</p>
                             <p className={'places-upper__text-down'}>вашего настроения <img src={arrowRightLong} alt={'arrow left'}/></p>
                         </div>
-                        <div>
+                        <div className={'places-upper__cards'}>
                             <div className={'places-card'}>
-                                <img  />
-                                <div>
-                                    <p></p>
+                                <img src={cartLounge} alt={'cart lounge'} />
+                                <div className={'places-card__down'}>
+                                    <p>CART Lounge</p>
                                     <p><img src={location} alt={'location'} />ул. 2-я Юго-Западная д.3А</p>
                                 </div>
                             </div>
-                            <div className={'places-card'}></div>
+                            <div className={'places-card'}>
+                                <img src={meraKitchen} alt={'cart lounge'} />
+                                <div className={'places-card__down'}>
+                                    <p>Mera kitchen</p>
+                                    <p><img src={location} alt={'location'} />ул. Разведчика Ахмерова, 7</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <div className={'places-card'}></div>
-                        <div className={'places-card'}></div>
-                        <div className={'places-card'}></div>
-                        <div className={'places-card'}></div>
+                    <div className={'places-down'}>
+                        <div className={'places-card'}>
+                            <img src={azu} alt={'cart lounge'} />
+                            <div className={'places-card__down'}>
+                                <p>AZU</p>
+                                <p><img src={location} alt={'location'} />ул. Декабристов, 8</p>
+                            </div>
+                        </div>
+                        <div className={'places-card'}>
+                            <img src={artel} alt={'cart lounge'} />
+                            <div className={'places-card__down'}>
+                                <p>ARTEL</p>
+                                <p><img src={location} alt={'location'} />ул. Муштари, 18</p>
+                            </div>
+                        </div>
+                        <div className={'places-card'}>
+                            <img src={vanGofh} alt={'cart lounge'} />
+                            <div className={'places-card__down'}>
+                                <p>Van Gofh</p>
+                                <p><img src={location} alt={'location'} />п. Ибрагимова, 56А, этаж 3</p>
+                            </div>
+                        </div>
+                        <div className={'places-card'}>
+                            <img src={cheeseria} alt={'cart lounge'} />
+                            <div className={'places-card__down'}>
+                                <p>Cheeseria</p>
+                                <p><img src={location} alt={'location'} />ул. Мусы Джалиля, 8/44</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
+            <div className={'container'}>
+                <p>Прогулка по татарским слободам</p>
+
+            </div>
+
             <div className={"container"}>
                 <p>Чем заняться в Казани</p>
+            </div>
+
+            <div className={'container'}>
+                <div className={'form'}>
+                    <div>
+                        <img src={formCloud} alt={'cloud'} />
+                    </div>
+                    <div className={'form-right'}>
+                        <p className={'form-right__title'}>Путеводитель по Казани</p>
+                        <p className={'form-right__subtitle'}>Заполните форму и мы с вами свяжемся</p>
+                        <div className={'form-right__inputs'}>
+                            <input placeholder={'Имя'} />
+                            <input placeholder={'E-mail'}  />
+                        </div>
+                        <div className={'form-right__button'}>
+                            <button>Отправить</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className={"container"}>
